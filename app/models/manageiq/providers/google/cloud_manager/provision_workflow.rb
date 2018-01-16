e28416897cda3092251a34001d3b4c3776d902e3
d-m-u <drewuhlmann@gmail.com>
2018-01-16 11:51:58 -0500

    Extend allowed_cloud_network for providers that don't support allowed_ci

--- app/models/manageiq/providers/google/cloud_manager/provision_workflow.rb
+++ app/models/manageiq/providers/google/cloud_manager/provision_workflow.rb
@@ -5,6 +5,14 @@ class ManageIQ::Providers::Google::CloudManager::ProvisionWorkflow < ::MiqProvis
     ems.each_with_object({}) { |f, h| h[f.id] = display_name_for_name_description(f) }
   end
 
+  def allowed_cloud_networks(_options = {})
+    return {} unless (src_obj = provider_or_tenant_object)
+
+    src_obj.all_cloud_networks.each_with_object({}) do |cn, hash|
+      hash[cn.id] = cn.cidr.blank? ? cn.name : "#{cn.name} (#{cn.cidr})"
+    end
+  end
+
   private
 
   def dialog_name_from_automate(message = 'get_dialog_name')
