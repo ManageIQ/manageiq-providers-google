190f0644fd19d56bf6a501a3c0f9997450e39871
Adam Grare <agrare@redhat.com>
2019-07-25 16:13:25 -0400

    Remove legacy refresh option
    Remove the option to switch back to legacy refresh and delete the old
    code.

--- app/models/manageiq/providers/google/cloud_manager/refresher.rb
+++ app/models/manageiq/providers/google/cloud_manager/refresher.rb
@@ -1,9 +1,5 @@
 module ManageIQ::Providers::Google
   class CloudManager::Refresher < ManageIQ::Providers::BaseManager::Refresher
-    def parse_legacy_inventory(ems)
-      ManageIQ::Providers::Google::CloudManager::RefreshParser.ems_inv_to_hashes(ems, refresher_options)
-    end
-
     def save_inventory(ems, _targets, hashes)
       super
       EmsRefresh.queue_refresh(ems.network_manager)
