50e24c18a85d346faa89bf61976e571ec211fad2
Adam Grare <agrare@redhat.com>
2020-11-03 09:31:52 -0500

    Add CloudVolume subclass under Google::CloudManager

--- app/models/manageiq/providers/google/cloud_manager.rb
+++ app/models/manageiq/providers/google/cloud_manager.rb
@@ -1,6 +1,7 @@
 class ManageIQ::Providers::Google::CloudManager < ManageIQ::Providers::CloudManager
   require_nested :AuthKeyPair
   require_nested :AvailabilityZone
+  require_nested :CloudVolume
   require_nested :EventCatcher
   require_nested :EventParser
   require_nested :Flavor
