b7ab35114fea20c48bef2c94893fd17e0499e347
Jason Frey <fryguy9@gmail.com>
2021-01-15 11:42:16 -0500

    Move logger initialization and configuration into plugins

--- lib/manageiq/providers/google/engine.rb
+++ lib/manageiq/providers/google/engine.rb
@@ -13,6 +13,14 @@ module ManageIQ
         def self.plugin_name
           _('Google Provider')
         end
+
+        def self.init_loggers
+          $gce_log ||= Vmdb::Loggers.create_logger("gce.log")
+        end
+
+        def self.apply_logger_config(config)
+          Vmdb::Loggers.apply_config_value(config, $gce_log, :level_gce)
+        end
       end
     end
   end
