0f262c3775035975a91129f6f86838d8ae1fdcbe
Jason Frey <fryguy9@gmail.com>
2021-03-17 14:54:34 -0400

    Merge pull request #182 from agrare/use_core_combined_refresh
    Use combined refresh support from core
    
    (cherry picked from commit d6363eed10e3c82c3fc6a97f19ba2a794d089b01)

--- app/models/manageiq/providers/google/inventory/persister/definitions/cloud_collections.rb
+++ app/models/manageiq/providers/google/inventory/persister/definitions/cloud_collections.rb
@@ -2,20 +2,21 @@ module ManageIQ::Providers::Google::Inventory::Persister::Definitions::CloudColl
   extend ActiveSupport::Concern
 
   def initialize_cloud_inventory_collections
-    %i(availability_zones
-       disks
-       flavors
-       hardwares
-       operating_systems
-       miq_templates
-       vms).each do |name|
-
-      add_collection(cloud, name)
+    %i[
+      availability_zones
+      cloud_volumes
+      disks
+      flavors
+      hardwares
+      operating_systems
+      miq_templates
+      vms
+    ].each do |name|
+      add_cloud_collection(name)
     end
 
     add_auth_key_pairs
 
-    add_cloud_volumes
     add_cloud_volume_snapshots
 
     add_advanced_settings
@@ -28,24 +29,12 @@ module ManageIQ::Providers::Google::Inventory::Persister::Definitions::CloudColl
 
   # ------ IC provider specific definitions -------------------------
 
-  def add_cloud_volumes
-    add_collection(cloud, :cloud_volumes) do |builder|
-      builder.add_default_values(
-        :ems_id => manager.id
-      )
-    end
-  end
-
   def add_cloud_volume_snapshots
-    add_collection(cloud, :cloud_volume_snapshots) do |builder|
-      builder.add_default_values(
-        :ems_id => manager.id
-      )
-    end
+    add_cloud_collection(:cloud_volume_snapshots)
   end
 
   def add_auth_key_pairs
-    add_collection(cloud, :auth_key_pairs) do |builder|
+    add_cloud_collection(:auth_key_pairs) do |builder|
       builder.add_properties(
         :manager_ref => %i(name fingerprint),
       )
@@ -54,7 +43,7 @@ module ManageIQ::Providers::Google::Inventory::Persister::Definitions::CloudColl
 
   # advanced_settings for VMs
   def add_advanced_settings
-    add_collection(cloud, :vms_and_templates_advanced_settings) do |builder|
+    add_cloud_collection(:vms_and_templates_advanced_settings) do |builder|
       builder.add_properties(
         :manager_ref                  => %i(resource),
         :model_class                  => ::AdvancedSetting,
