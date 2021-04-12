5192085e0f716984292fec9f81e4f0dd0f0b537f
Adam Grare <adam@grare.com>
2021-04-12 11:04:48 -0400

    Use pluggable provider secrets

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,17 +12,8 @@ VCR.configure do |config|
   config.ignore_hosts 'codeclimate.com' if ENV['CI']
   config.cassette_library_dir = File.join(ManageIQ::Providers::Google::Engine.root, 'spec/vcr_cassettes')
 
-  # Looks for provider subkeys you set in secrets.yml. Replace the values of
-  # those keys (both escaped or unescaped) with some placeholder text.
-  require "cgi"
   secrets = Rails.application.secrets
-  secrets.each_key do |provider|
-    next if %i(secret_key_base secret_token).include?(provider) # Defaults
-
-    cred_hash = secrets.public_send(provider)
-    cred_hash.each do |key, value|
-      config.filter_sensitive_data("#{provider.upcase}_#{key.upcase}") { CGI.escape(value) }
-      config.filter_sensitive_data("#{provider.upcase}_#{key.upcase}") { value }
-    end
+  secrets.google.keys do |secret|
+    config.define_cassette_placeholder(secrets.google_defaults[secret]) { secrets.google[secret] }
   end
 end
