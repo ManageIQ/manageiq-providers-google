259bde9b77c2bea457e1f25e8d0d8c16f83be8ab
Adam Grare <adam@grare.com>
2022-01-10 11:15:36 -0500

    Add missing GKE subclasses

---
+++ app/models/manageiq/providers/google/container_manager/container_volume.rb
@@ -0,0 +1,4 @@
+ManageIQ::Providers::Kubernetes::ContainerManager::ContainerVolume.include(ActsAsStiLeafClass)
+
+class ManageIQ::Providers::Google::ContainerManager::ContainerVolume < ManageIQ::Providers::Kubernetes::ContainerManager::ContainerVolume
+end
---
+++ app/models/manageiq/providers/google/container_manager/service_instance.rb
@@ -0,0 +1,4 @@
+ManageIQ::Providers::Kubernetes::ContainerManager::ServiceInstance.include(ActsAsStiLeafClass)
+
+class ManageIQ::Providers::Google::ContainerManager::ServiceInstance < ManageIQ::Providers::Kubernetes::ContainerManager::ServiceInstance
+end
