2d2d155119e86591ab6b52fc217bd548242cf9bf
Adam Grare <adam@grare.com>
2025-06-24 11:22:46 -0400

    Add ContainerProject Subclass

---
+++ app/models/manageiq/providers/google/container_manager/container_project.rb
@@ -0,0 +1,4 @@
+ManageIQ::Providers::Kubernetes::ContainerManager::ContainerProject.include(ActsAsStiLeafClass)
+
+class ManageIQ::Providers::Google::ContainerManager::ContainerProject < ManageIQ::Providers::Kubernetes::ContainerManager::ContainerProject
+end
